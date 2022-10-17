d102f9780cd133cadc5a804e7f9f874a0a3d1254
bigodom <pedro_augusto50@hotmail.com>
2022-10-16 21:12:15 -0300

    grafico 2

--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,7 @@ function teste() {
 
 function ec(){
   // Initialize the echarts instance based on the prepared dom
-  var myChart = echarts.init(document.getElementById('main'));
+  var myChart = echarts.init(document.getElementById('grafico1'));
   // Specify the configuration items and data for the chart
   var option = {
     title: {
@@ -38,4 +38,28 @@ function ec(){
 
   // Display the chart using the configuration items and data just specified.
   myChart.setOption(option);
+}
+{
+  var chartDom = document.getElementById('grafico2');
+  var myChart = echarts.init(chartDom);
+  var option;
+
+  option = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', '2021']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1330, 1188],
+        type: 'line',
+        smooth: true
+      }
+    ]
+  };
+
+  option && myChart.setOption(option);
 }
