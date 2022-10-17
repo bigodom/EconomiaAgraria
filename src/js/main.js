26fcf217886e639771b4dd2d2fd1d3524390fafa
bigodom <pedro_augusto50@hotmail.com>
2022-10-17 00:55:49 -0300

    graficos dark

--- src/js/main.js
+++ src/js/main.js
@@ -41,20 +41,20 @@ function ec(){
 }
 {
   var chartDom = document.getElementById('grafico2');
-  var myChart = echarts.init(chartDom);
+  var myChart = echarts.init(chartDom, 'dark');
   var option;
 
   option = {
     xAxis: {
       type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', '2021']
+      data: ['1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021']
     },
     yAxis: {
       type: 'value'
     },
     series: [
       {
-        data: [820, 932, 901, 934, 1290, 1330, 1188],
+        data: [264, 243, 294,	306,	306,	420,	461,	532,	594,	605,	553,	549,	629,	716,	689,	717,	806,	842,	924,	955,	964,	960, 956,	927,	945, 1106, 1221],
         type: 'line',
         smooth: true
       }
@@ -62,4 +62,48 @@ function ec(){
   };
 
   option && myChart.setOption(option);
+}
+{
+  var chartDom = document.getElementById('grafico3');
+var myChart = echarts.init(chartDom, 'dark');
+var option;
+
+option = {
+  title: {
+    text: 'Participação das regiões no VBP em 2021',
+    subtext: 'valores em %',
+    left: 'center'
+  },
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    orient: 'vertical',
+    left: 'left'
+  },
+  series: [
+    {
+      name: 'Participação do:',
+      type: 'pie',
+      radius: '50%',
+      data: [
+        { value: 6.3, name: 'Norte' },
+        { value: 9.7, name: 'Nordeste' },
+        { value: 23.4, name: 'Sul' },
+        { value: 26.6, name: 'Sudeste' },
+        { value: 34.12, name: 'Centro-Oeste' }
+      ],
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }
+  ]
+};
+
+option && myChart.setOption(option);
+
 }
